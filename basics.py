5cf17f755ffa836dcc672f84e5c61cfe6cf1ff0e
n i s h i t a <nishita300601@gmail.com>
2020-10-22 17:23:01 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -13,7 +13,6 @@ def browseFunc():
     root = Tk()
     root.withdraw()
     file_path = filedialog.askopenfilename()
-    #file_path = askopenfilename()
     return file_path
     
 def writeFile():
@@ -24,24 +23,33 @@ def writeFile():
     file.close()
 
 
-
-
-
-
+#create a window
 window = Tk()
+
+#set a title
 window.title("Lab-3")
-#txt = browseFunc()
+
+#give a heading
+label1 = Label(window,text="lab-3 group assignment\n\n")
+label1.grid(row=7,column=1)
+
+#instructions for the entry field
+label2 = Label(window,text="enter some sentence to be written to the file\n")
+label2.grid(row=8,column=1)
+
+#some input text is taken here, which will be written to a file
 metinF = Entry(window)
 metinF.grid(row=9, column=1)
 
+#clicking this button gives a prompt to choose a file,where the previous text will be written
 butonWrite = Button(window)
-butonWrite.config(text = 'Write To File', command = writeFile)
-butonWrite.grid(row=8, column=1)
+butonWrite.config(text = 'Choose & Write To File', command = writeFile)
+butonWrite.grid(row=10, column=1)
 
-#label = Label(window,text="lab-3 group assignment").pack()
-#label = tkinter.Label(window,text=txt).pack()
-#bt = Button(window,text="Browse",command=browseFunc).pack()
+#size of the window
+window.geometry("500x200")
 
+#run the loop until stopped externally
 window.mainloop()
 
 
